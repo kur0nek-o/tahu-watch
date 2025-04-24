9c9ee7f663375210aedcf45b3d198ba8482e3ff8
kur0nek-o <maulana21mh@gmail.com>
2025-04-24 10:16:06 +0700

    feat: implements login and register without tailwindcss

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="form-box">
+      <div class="logo"></div>
+      <h2>Register</h2>
+      <form>
+        <input type="email" placeholder="Email" required />
+        <input type="text" placeholder="Username" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Daftar</button>
+      </form>
+      <p>Sudah punya akun? <RouterLink to="/">Login</RouterLink></p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
